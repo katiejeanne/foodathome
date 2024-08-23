017f7b915608f4c7c80b89588729e64302e63c36
Katie Wessman <kjeanne@gmail.com>
2024-08-22 20:20:06 -0500

    Correct DataInitalizer class to use different test profile.

--- src/test/java/dev/katiejeanne/foodathome/init/DataInitializerTests.java
+++ src/test/java/dev/katiejeanne/foodathome/init/DataInitializerTests.java
@@ -7,12 +7,13 @@ import dev.katiejeanne.foodathome.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("inittest")
 public class DataInitializerTests {
 
     @Autowired
